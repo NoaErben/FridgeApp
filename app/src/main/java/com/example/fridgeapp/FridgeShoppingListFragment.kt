dffdcddd1ec447579836e8a4c506b33bfb6aa418
Liraz gabbay <liraz.gabby@runi.ac.il>
2024-05-25 17:43:43 +0300

    pop up menu for main fragment + fridge shopping + main + vectors

--- app/src/main/java/com/example/fridgeapp/FridgeShoppingListFragment.kt
+++ app/src/main/java/com/example/fridgeapp/FridgeShoppingListFragment.kt
@@ -7,26 +7,19 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.fridgeapp.databinding.DefaultExpirationDatesBinding
 
-class DefaultExpirationDatesFragment: Fragment (){
+class FridgeShoppingListFragment : Fragment() {
+    private var _binding: DefaultExpirationDatesBinding? = null
+    private val binding get() = _binding!!
 
-    private  var _binding : DefaultExpirationDatesBinding? = null
-    private val binding
- get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        _binding = DefaultExpirationDatesBinding.inflate(inflater,container,false)
-        binding.addProductExpiryBtn.setOnClickListener {
-
-        }
+    ): View {
+        _binding = DefaultExpirationDatesBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
