e17331c2f8f9d37a1841fcd5faf8e8a7cfe3b28a
GuyGal <guy.gal@post.runi.ac.il>
2024-05-31 17:17:46 +0300

    FB gradle and libs update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
-    id("kotlin-kapt") // kapt
+    id("kotlin-kapt")
+    id("com.google.gms.google-services") // kapt
 }
 
 android {
@@ -52,6 +53,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
