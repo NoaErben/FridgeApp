d47ebfd34009f87b2f65051a46026cdf49489f69
GuyGal <guy.gal@post.runi.ac.il>
2024-05-31 17:17:21 +0300

    FridgeManagerFragment basic binding

--- app/src/main/java/com/example/fridgeapp/FridgeManagerFragment.kt
+++ app/src/main/java/com/example/fridgeapp/FridgeManagerFragment.kt
@@ -1,6 +1,23 @@
 package com.example.fridgeapp
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.fridgeapp.databinding.FridgeFragmentBinding
 
 class FridgeManagerFragment: Fragment() {
+
+    private var _binding: FridgeFragmentBinding? = null
+    private val binding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FridgeFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
