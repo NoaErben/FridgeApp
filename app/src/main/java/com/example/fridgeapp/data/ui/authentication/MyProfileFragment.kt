2893e4ff7e9df0bcf6d2179ad36c382bc565599a
Noa Erben <noa.erben@post.runi.ac.il>
2024-06-13 15:57:23 +0300

    fixed style

--- app/src/main/java/com/example/fridgeapp/data/ui/authentication/MyProfileFragment.kt
+++ app/src/main/java/com/example/fridgeapp/data/ui/authentication/MyProfileFragment.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.text.SpannableString
+import android.text.TextPaint
 import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
 import android.view.LayoutInflater
@@ -11,6 +12,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
@@ -69,11 +71,17 @@ class MyProfileFragment: Fragment() {
             val url = "https://www.google.com/maps/search/?api=1&query=$encodedQuery"
             binding.tvGoogleMapsLink.text = url
 
-            val spannableString = SpannableString("Find supermarkets near you")
+            val spannableString = SpannableString("Click here to find supermarkets nearby")
             val clickableSpan = object : ClickableSpan() {
                 override fun onClick(widget: View) {
                     val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
                     startActivity(intent)
+                }
+
+                override fun updateDrawState(ds: TextPaint) {
+                    super.updateDrawState(ds)git
+                    ds.color = ContextCompat.getColor(requireContext(), R.color.white) // Set to white color
+                    ds.isUnderlineText = true // Optional, remove if you don't want the text to be underlined
                 }
             }
             spannableString.setSpan(clickableSpan, 0, spannableString.length, 0)
