062acb12999106cb5b0a7eb2822e84d4891a7a5d
Noa Erben <noa.erben@post.runi.ac.il>
2024-06-13 09:49:51 +0300

    moved authentication to fbViewModel

--- app/src/main/java/com/example/fridgeapp/data/ui/authentication/RegisterFragment.kt
+++ app/src/main/java/com/example/fridgeapp/data/ui/authentication/RegisterFragment.kt
@@ -1,6 +1,5 @@
 package com.example.fridgeapp.data.ui.authentication
 
-
 import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -13,19 +12,15 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.fridgeapp.R
-import com.example.fridgeapp.data.model.User
 import com.example.fridgeapp.data.ui.viewModels.FbViewModel
 import com.example.fridgeapp.databinding.AuthRegisterBinding
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
 
 class RegisterFragment : Fragment() {
 
     private var _binding: AuthRegisterBinding? = null
     private val binding get() = _binding!!
     private lateinit var auth: FirebaseAuth
-    private lateinit var databaseReference: DatabaseReference
     private lateinit var dialog: Dialog
 
     private val fbViewModel: FbViewModel by activityViewModels()
@@ -41,9 +36,7 @@ class RegisterFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         auth = FirebaseAuth.getInstance()
-        databaseReference = FirebaseDatabase.getInstance().getReference("Users")
 
-        // Set click listener for login button
         binding.btnSubmit.setOnClickListener {
             val email = binding.etEmail.text.toString().trim()
             val name = binding.etName.text.toString().trim()
@@ -73,7 +66,6 @@ class RegisterFragment : Fragment() {
                 return@setOnClickListener
             }
 
-            // If passwords are valid, set the text color to green
             val greenColor = ContextCompat.getColor(requireContext(), R.color.green)
             binding.etPassword.setTextColor(greenColor)
             binding.etConfirmPassword.setTextColor(greenColor)
@@ -81,18 +73,13 @@ class RegisterFragment : Fragment() {
             showProgressBar()
             auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
                 if (task.isSuccessful) {
-                    val uid = auth.currentUser?.uid
-                    if (uid != null) {
-                        val user = User(name, email, uid)
-                        databaseReference.child(uid).setValue(user).addOnCompleteListener { userTask ->
-                            if (userTask.isSuccessful) {
-                                hideProgressBar()
-                                Toast.makeText(requireContext(), "User registered successfully", Toast.LENGTH_SHORT).show()
-                                findNavController().navigate(R.id.action_registerFragment_to_fridgeManagerFragment)
-                            } else {
-                                hideProgressBar()
-                                Toast.makeText(requireContext(), "Failed to register user in database", Toast.LENGTH_SHORT).show()
-                            }
+                    fbViewModel.saveUserToDatabase { result ->
+                        hideProgressBar()
+                        if (result.isSuccess) {
+                            Toast.makeText(requireContext(), "User registered successfully", Toast.LENGTH_SHORT).show()
+                            findNavController().navigate(R.id.action_registerFragment_to_fridgeManagerFragment)
+                        } else {
+                            Toast.makeText(requireContext(), "Failed to save user: ${result.exceptionOrNull()?.message}", Toast.LENGTH_SHORT).show()
                         }
                     }
                 } else {
